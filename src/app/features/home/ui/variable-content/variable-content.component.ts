68f13b70af331cdde19e0221f91a0858d11951f2
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2024-06-04 02:05:24 +0200

    ad git + fix stupdo

--- src/app/features/home/ui/variable-content/variable-content.component.ts
+++ src/app/features/home/ui/variable-content/variable-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { SubscriberComponent } from "../../../../shared/components/subscriber/subscriber.component";
 import { HighlightService } from "../../../../shared/utils/highlightService";
@@ -11,22 +11,15 @@ import { HighlightService } from "../../../../shared/utils/highlightService";
   styleUrls: ['./variable-content.component.css']
 })
 export class VariableContentComponent extends SubscriberComponent implements AfterViewInit {
-  @ViewChild('highlightText') highlightText!: ElementRef;
-  @ViewChild('highlightText2') highlightText2!: ElementRef;
-  @ViewChild('highlightText3') highlightText3!: ElementRef;
-  @ViewChild('highlightText4') highlightText4!: ElementRef;
-  @ViewChild('highlightText5') highlightText5!: ElementRef;
+  @ViewChildren('highlightText') highlightTexts!: QueryList<ElementRef>;
 
   constructor(private highlightService: HighlightService) {
     super();
   }
 
   ngAfterViewInit() {
-    this.highlightService.addHighlightEffect(this.highlightText.nativeElement);
-    this.highlightService.addHighlightEffect(this.highlightText2.nativeElement);
-    this.highlightService.addHighlightEffect(this.highlightText3.nativeElement);
-    this.highlightService.addHighlightEffect(this.highlightText4.nativeElement);
-    this.highlightService.addHighlightEffect(this.highlightText5.nativeElement);
-
+    this.highlightTexts.forEach(highlightText => {
+      this.highlightService.addHighlightEffect(highlightText.nativeElement);
+    });
   }
 }
